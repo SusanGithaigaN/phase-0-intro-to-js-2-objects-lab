6416f72ce2e6228e7a6be9730eb4d0614b1ed38f
SusanGithaiga <susan.githaiga@student.moringaschool.com>
2022-11-23 16:26:20 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Write your solution in this file!
+const employee = {
+    name:'Jared',
+    streetAddress:34
+};
+//Return a clone with the new data without modifying original employee
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    const employeeRecords = {...employee};
+    employeeRecords[key ] = value;
+    return employeeRecords;
+    }
+// Destructively update employee and return updated employee
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key] = value;
+    return employee;
+}
+// Delete key from employee clone key and return new employee
+function deleteFromEmployeeByKey(employee, key){ 
+    const newEmployee = {...employee};
+        newEmployee[key] = undefined;
+    return newEmployee;
+}
+// Return employee without the deleted key 
+//Modify employee
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key];
+    
+    return employee;
+}
